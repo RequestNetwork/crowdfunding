2da1f41b98786a7eb3c0355f2d667f25af6acc31
Benjamin Shafii <benjamin.shafii@gmail.com>
2018-09-13 18:00:53 +0200

    Increase project creation speed (and cost)

--- src/containers/RequestNetwork/index.js
+++ src/containers/RequestNetwork/index.js
@@ -33,7 +33,10 @@ const createRequest = (
     '0x7d085bb0b1b2c99b42e3b17a4609311e4518208c', // aribtrary payer address
     paymentAddresses,
     undefined, // refund
-    JSON.stringify(data)
+    JSON.stringify(data),
+    undefined, // extensions
+    undefined, // extensionsParams
+    {gasPrice: '15000000000'}
   );
 
 const payRequest = (requestNetwork, requestId, amounts) =>
