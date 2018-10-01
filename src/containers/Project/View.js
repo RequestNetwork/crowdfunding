7b7f53f5ecf8d678788ed99cf04f742ff46cbfc4
Benjamin Shafii <benjamin.shafii@gmail.com>
2018-10-01 18:12:19 +0200

    Remove reference to previous implementation

--- src/containers/Project/View.js
+++ src/containers/Project/View.js
@@ -5,7 +5,6 @@ import { ViewHeader } from './Header';
 import { Loader } from '../../components/Loader';
 import { SideBar } from './SideBar';
 import { RequestInfo } from './SideBar/RequestInfo';
-import { Backer as OldBacker } from './SideBar/Backer';
 import { Backer } from '@requestnetwork/react-components';
 import BackerSection from './SideBar/BackerSection';
 
